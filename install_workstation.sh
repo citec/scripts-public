f2c888a6bfc1bd9990b6cbdb65f787d2a0cccce8
Jaime A. Sánchez Hidalgo <jaime@grupocitec.com>
2013-11-13 01:04:42 -0300

    Change commans order in install_workstation.sh

--- install_workstation.sh
+++ install_workstation.sh
@@ -15,27 +15,27 @@ fi
 # Gather some data
 oldhostname=$(cat /etc/hostname)
 
-# Install puppet and facter
-echo "Installing puppet..."
-apt-get install -y puppet facter > /dev/null
-echo "DONE"
-
 # Change hostname
-echo "Enter new hostname: "
+printf "Enter new hostname: "
 read newHostname
 echo $newHostname > /etc/hostname
 sed -i "s/$oldhostname/$newHostname/g" /etc/hosts
 service hostname restart
 
+# Install puppet and facter
+echo "Installing puppet... "
+apt-get install -y puppet facter > /dev/null
+echo "DONE"
+
 # Connect to puppet master 
-printf "Connection to puppet master to request authentication..."
+printf "Connection to puppet master to request authentication... "
 puppet agent --server=puppet.grupocitec.com --no-daemonize --onetime 2>&1 > /dev/null
 echo "DONE"
 printf "Now go to GrupoCITEC's OpenERP and authorize your host, then press enter"
 read ENTER
 
 # Connect to puppet master 
-echo "Installing workstation..."
+echo "Installing workstation... "
 puppet agent --server=puppet.grupocitec.com --no-daemonize --onetime --verbose
 echo "DONE"
 
