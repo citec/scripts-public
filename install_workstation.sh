6bb432cd3cbb0e80d219325cfa4f2f8483fbc5ff
Jaime A. Sánchez Hidalgo <jaime@grupocitec.com>
2014-01-05 12:45:43 -0300

    Add some checks before running some pieces of code

--- install_workstation.sh
+++ install_workstation.sh
@@ -18,15 +18,29 @@ fi
 oldhostname=$(cat /etc/hostname)
 
 # Change hostname
-printf "Enter new hostname: "
-read newHostname
-echo $newHostname > /etc/hostname
-sed -i "s/$oldhostname/$newHostname/g" /etc/hosts
-service hostname restart
+read -r -p "Do you want to change hostname? [Y/n]" response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]]
+    then
+        printf "Enter new hostname: "
+        read newHostname
+        echo $newHostname > /etc/hostname
+        sed -i "s/$oldhostname/$newHostname/g" /etc/hosts
+        service hostname restart
+fi
 
 # Install puppet and facter
-echo "Installing puppet... "
-apt-get install -y puppet facter > /dev/null
+command -v puppet >/dev/null 2>&1 || { 
+    echo "Installing puppet... "
+    apt-get install -y puppet facter > /dev/null
+    echo "DONE"
+    }
+command -v facter >/dev/null 2>&1 || { 
+    echo "Installing facter... "
+    apt-get install -y facter > /dev/null
+    echo "DONE"
+    }
+echo "Stopping puppet service... "
 service puppet stop
 puppet agent --enable --no-daemonize
 echo "DONE"
@@ -35,9 +49,9 @@ set +e
 
 # Connect to puppet master 
 printf "Connection to puppet master to request authentication... "
-puppet agent --server=puppet.grupocitec.com --no-daemonize --onetime 2>&1 > /dev/null
+puppet agent --server=puppet.grupocitec.com --no-daemonize --onetime #2>&1 > /dev/null
 echo "DONE"
-printf "Now go to GrupoCITEC's OpenERP and authorize your host, then press enter"
+printf "Now go to GrupoCITEC's OpenERP and authorize your host, then press enter here"
 read ENTER
 
 # Connect to puppet master 
